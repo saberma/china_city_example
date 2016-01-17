e57aa13584c2769bc94eea4fdbdfb4323091af2f
saberma <mahb45@gmail.com>
2016-01-17 13:31:41 +0800

    it works.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   root 'home#page'
 
+  mount ChinaCity::Engine => '/china_city'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
